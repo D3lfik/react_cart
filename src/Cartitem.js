674fe8185146e54f0ed6377d436454d1a649a4e3
Apurv <apurv.mod007@gmail.com>
2023-08-04 19:26:34 +0530

    add increaseQuantity func

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,8 +1,23 @@
 import React from "react";      
 import App from "./App";
+import { FaPlus } from "react-icons/fa6";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price:999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img:''
+        }
+       // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity =() => {
+        console.log('this.test', this.state);
+    }
     render(){
+        const { price , title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -10,11 +25,23 @@ class CartItem extends React.Component{
 
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{color: '#777'}}>Rs:{price}</div>
+                    <div style={{color: '#777'}}>Qty:{qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons*/} </div>
+                        <img alt='increase' 
+                        className="actions-icons"
+                        src = ""
+                        onClick={this.increaseQuantity}
+                         />
+                        <img alt='decrease' 
+                        className="actions-icons" src=""
+                        />
+                        <img alt='delete' 
+                        className="actions-icons" 
+                        src=""
+                        />
 
                 </div>
 
