93672ce2caa3cddbf42d688d53d5963e7d93f41d
Apurv <apurv.mod007@gmail.com>
2023-08-25 22:20:01 +0530

    Onsnapshot

--- src/App.js
+++ src/App.js
@@ -15,10 +15,27 @@ class App extends React.Component {
     }
 
     componentDidMount(){
+      // firestore
+      // .collection('products')
+      // .get()
+      // .then((snapshot)=>{
+      //   console.log(snapshot);
+      //   snapshot.docs.map((doc) =>{
+      //     console.log(doc.data());
+      //   });
+      //   const products = snapshot.docs.map((doc)=>{
+      //     const data = doc.data();
+      //     data['id'] = doc.id;
+      //     return data;
+      //   })
+      //   this.setState({
+      //     products ,
+      //     loading:false
+      //   })
+      // })
       firestore
       .collection('products')
-      .get()
-      .then((snapshot)=>{
+      .onSnapshot((snapshot)=>{
         console.log(snapshot);
         snapshot.docs.map((doc) =>{
           console.log(doc.data());
